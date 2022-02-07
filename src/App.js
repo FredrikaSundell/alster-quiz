936b59219892bd8cb0e8fea832106b98d64f2829
fredrikasundell <fredrikasundell@gmail.com>
2022-02-07 20:02:32 +0100

    import Navigate for when path No Match

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.scss'
 import Start from './components/start/Start'
 import Quiz from './components/quiz/Quiz'
@@ -11,6 +11,8 @@ function App() {
         <Route path="/" element={<Start />} />
         <Route path="quiz" element={<Quiz />} />
         <Route path="results" element={<Results />} />
+        //redirect on the initial render in the client
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </div>
   )
