fd0daa09a6fb081b121b052f4314215ab0110c67
fredrikasundell <fredrikasundell@gmail.com>
2022-02-15 20:39:20 +0100

    shows question

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import Button from '../buttons/Button'
+import useStore from '../../hooks/useStore'
+// import RadioButton from '../radioButton/RadioButton'
 
 function Quiz() {
+  const questions = useStore((state) => state.questions)
+  console.log(questions)
+  const currentQuestion = useStore(
+    (state) => state.questions[state.currentQuestionIndex]
+  )
+  console.log(currentQuestion, 222)
+
   return (
     <div>
-      <h1>Hallå från Quiz</h1>
+      <h1>{currentQuestion.question}</h1>
+
+      {/* <RadioButton /> */}
+
       <Button className="results-btn" to="/results">
-        Results
+        Resultat
       </Button>
     </div>
   )
--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,9 +1,13 @@
 //Installerat zustand
+import data from '../data/data.json'
 import create from 'zustand'
 //Skapar globalstore
-const useStore = create((set) => ({
+const useStore = create((set, get) => ({
   userName: '',
   setUserName: (name) => set({ userName: name }),
+  //hämtar nuvarande fråga
+  questions: data,
+  currentQuestionIndex: 0,
 }))
 
 export default useStore
