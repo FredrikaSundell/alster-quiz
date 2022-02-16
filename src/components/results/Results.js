f420e19ce7ec14a320888377da665d58f07d52f9
fredrikasundell <fredrikasundell@gmail.com>
2022-02-16 22:49:38 +0100

    show result

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -2,10 +2,23 @@ import useStore from '../../hooks/useStore'
 
 function Results() {
   const userName = useStore((state) => state.userName)
+  const questions = useStore((state) => state.questions)
+
+  const result = questions.reduce((acc, current) => {
+    const correctAnswer = current.options.find((opt) => opt.isCorrect).value
+    if (current.answer === correctAnswer) {
+      return acc + 1
+    } else {
+      return acc
+    }
+  }, 0)
+
   return (
     <div>
       <h1>GRATTIS {userName} DU FICK!</h1>
-      <p>resultat</p>
+      <p>
+        {result} av {questions.length}
+      </p>
       <button>Spela igen</button>
     </div>
   )
